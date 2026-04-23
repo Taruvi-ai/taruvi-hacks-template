3c13572d814967dda473ab7c00937c7b5846fd15
Ankitha Lakshmisha <alakshmisha@eoxvantage.com>
2026-04-23 19:35:14 -0400

    console logging outside mount

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,25 @@ import "./utils/clientLogger";
 
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import App from "./App";
+import { ConsoleLogDrawer } from "./components";
 
 const container = document.getElementById("root") as HTMLElement;
-const root = createRoot(container);
-
-root.render(
+createRoot(container).render(
   <React.StrictMode>
     <App />
+  </React.StrictMode>
+);
+
+const drawerContainer = document.createElement("div");
+drawerContainer.id = "error-drawer-root";
+document.body.appendChild(drawerContainer);
+createRoot(drawerContainer).render(
+  <React.StrictMode>
+    <ThemeProvider theme={createTheme()}>
+      <ConsoleLogDrawer />
+    </ThemeProvider>
   </React.StrictMode>
 );
